04bd5b1cebd5cdfbe598c4bde6102c091182e332
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-08-25 23:06:12 -0500

    Add EventServiceController providing REST API for listing events

---
+++ events/event-service-rest/src/main/java/org/rest/controllers/EventServiceController.java
@@ -0,0 +1,27 @@
+package org.rest.controllers;
+
+
+import org.api.services.EventService;
+import org.dto.events.Event;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventServiceController {
+
+    private final EventService eventService;
+
+    public EventServiceController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+}
