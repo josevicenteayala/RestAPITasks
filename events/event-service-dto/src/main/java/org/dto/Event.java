66ae8d21a72f0714eb2b58737e37b120208ff583
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-08-25 22:14:46 -0500

    Create event-service-dto module with Event class

---
+++ events/event-service-dto/src/main/java/org/dto/Event.java
@@ -0,0 +1,72 @@
+package org.dto;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private Long id;
+    private String title;
+    private String place;
+    private String speaker;
+    private String eventType;
+    private LocalDateTime dateTime;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", place='" + place + '\'' +
+                ", speaker='" + speaker + '\'' +
+                ", eventType='" + eventType + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+}
