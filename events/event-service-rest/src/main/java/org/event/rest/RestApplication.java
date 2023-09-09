b554e2024e244d6c9a6299d76d27f797d585a650
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-09-09 15:44:10 -0500

    Add componentScan

--- events/event-service-rest/src/main/java/org/event/rest/RestApplication.java
+++ events/event-service-rest/src/main/java/org/event/rest/RestApplication.java
@@ -2,8 +2,10 @@ package org.event.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"org.event", "org.event.api", "org.event.api.repository"})
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.event.api.repository","org.event.api","org.event"})
 public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class,args);
